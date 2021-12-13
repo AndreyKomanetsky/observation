1839383504d7b6b72d85b23a8300143cdf2fd8f6
AndreyKomanetsky <74355164+AndreyKomanetsky@users.noreply.github.com>
2021-12-13 14:59:15 +0300

    version 1.199

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ console.log('point 4')
 
 if (process.env.NODE_ENV === 'production') {
     console.log('point 5')
+//    app.use(express.static('client/build'))
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.use(express.static('client/build'))
-//    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-//
-//    app.get('*', (req, res) =>{
-//        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//    })
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 console.log('point 6')
 
